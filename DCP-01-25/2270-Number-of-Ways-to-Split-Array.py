a2ae2432c44e83e7b2a12ba195bee96ec45a066b
Iolanta Barysheva <82276707+yoylanta@users.noreply.github.com>
2025-01-03 22:51:31 +0300

    [2270] [Time Beats: 91.85%] [Memory Beats: 43.15%] - LeetPush

--- DCP-01-25/2270-Number-of-Ways-to-Split-Array.py
+++ DCP-01-25/2270-Number-of-Ways-to-Split-Array.py
@@ -1,11 +1,11 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        total=sum(nums)
+        right=(sum(nums)+1)//2
         leftsum=0
         splits=0
         for i in range (len(nums)-1):
             leftsum+=nums[i]
-            if leftsum>=(total - leftsum):
+            if leftsum>=right:
                 splits+=1
         return splits
         
