4df005c37a91e3ee699071906bc1a1c5c53c4b34
Iolanta Barysheva <82276707+yoylanta@users.noreply.github.com>
2025-01-05 01:00:30 +0300

    [242] [Time Beats: 98.77%] [Memory Beats: 34.94%] - LeetPush

--- 242-Valid-Anagram.py
+++ 242-Valid-Anagram.py
@@ -1,11 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        count=[0]*26
-        for letter in s:
-            count[ord(letter)-ord('a')]+=1
-        for letter in t:
-            count[ord(letter)-ord('a')]-=1
-        for value in count:
-            if value!=0:
+        if len(s) != len(t):
+            return False
+        for letter in set(s):
+            if s.count(letter) != t.count(letter):
                 return False
         return True
