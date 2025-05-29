879217b44eb0474842a664c1121c811a10aba0af
Iolanta Barysheva <82276707+yoylanta@users.noreply.github.com>
2025-05-29 11:07:38 +0300

    Time: 271 ms (97.96%), Space: 343.1 MB (48.98%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
+    void dfs(int node, int parent, int depth, const vector<vector<int>>& adj, vector<int>& color, int& even, int& odd) {
+        color[node] = depth % 2;
+        if (color[node] == 0) ++even;
+        else ++odd;
+        for (int neighbor : adj[node]) {
+            if (neighbor != parent) {
+                dfs(neighbor, node, depth + 1, adj, color, even, odd);
+            }
+        }
+    }
+
     vector<vector<int>> buildAdj(const vector<vector<int>>& edges) {
         int n = edges.size() + 1;
         vector<vector<int>> adj(n);
-        for (auto &e : edges) {
+        for (const auto& e : edges) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
         return adj;
     }
 
-    pair<vector<int>, pair<int, int>> bfsParity(const vector<vector<int>>& adj, int root) {
-        int n = adj.size();
-        vector<int> dist(n, -1);
-        queue<int> q;
-        q.push(root);
-        dist[root] = 0;
-        int even = 0, odd = 0;
-        while (!q.empty()) {
-            int u = q.front(); q.pop();
-            if (dist[u] % 2 == 0) ++even;
-            else ++odd;
-            for (int v : adj[u]) {
-                if (dist[v] == -1) {
-                    dist[v] = dist[u] + 1;
-                    q.push(v);
-                }
-            }
-        }
-        return {dist, {even, odd}};
-    }
-
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
         auto adj1 = buildAdj(edges1);
         auto adj2 = buildAdj(edges2);
-        auto [dist1, counts1] = bfsParity(adj1, 0);
-        int evenA = counts1.first, oddA = counts1.second;
-        auto [dist2, counts2] = bfsParity(adj2, 0);
-        int evenB = counts2.first, oddB = counts2.second;
+        int n = adj1.size(), m = adj2.size();
+        
+        vector<int> color1(n), color2(m);
+        int evenA = 0, oddA = 0, evenB = 0, oddB = 0;
+        
+        dfs(0, -1, 0, adj1, color1, evenA, oddA);
+        dfs(0, -1, 0, adj2, color2, evenB, oddB);
+        
         int maxB = max(evenB, oddB);
-        vector<int> res(adj1.size());
-        for (int i = 0; i < adj1.size(); ++i) {
-            res[i] = (dist1[i] % 2 == 0 ? evenA : oddA) + maxB;
+        vector<int> res(n);
+        for (int i = 0; i < n; ++i) {
+            res[i] = (color1[i] == 0 ? evenA : oddA) + maxB;
         }
         return res;
     }
