04479ae2d069b07c381d3ad7241a0e193f51fc98
Iolanta Barysheva <82276707+yoylanta@users.noreply.github.com>
2025-06-01 21:45:11 +0300

    Time: 0 ms (100%), Space: 9 MB (57.06%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
+    long long countWays(int sum) {
+        if (sum < 0) return 0;
+        return 1LL * (sum + 2) * (sum + 1) / 2;
+    }
+
     long long distributeCandies(int n, int limit) {
-        long long res=0;
-        for (int i=0; i<=min(limit,n);i++){
-            if(n-i<=2*limit)
-                res+=1+min(n-i,limit)-max(0,n-i-limit);
+        long long total = countWays(n);
+        for (int i = 1; i <= 3; ++i) {
+            int over = n - i * (limit + 1);
+            if (over < 0) break;
+            long long ways = countWays(over);
+            int coeff = (i % 2 == 1 ? -1 : 1) * (i == 3 ? 1 : 3);
+            total += coeff * ways;
         }
-        return res;
+        return total;;
     }
 };
