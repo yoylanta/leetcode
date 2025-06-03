7b5cfe189868355dbc517bd804e02c87ef32ecfb
Iolanta Barysheva <82276707+yoylanta@users.noreply.github.com>
2025-06-03 12:02:00 +0300

    Time: 0 ms (100%), Space: 44.1 MB (66.83%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-       queue<int> q;
-       unordered_set<int> have_keys;
-       unordered_set<int> have_boxes;
-       unordered_set<int> visited;
-       int total_candies=0;
+        const int n = status.size();
+        queue<int> q;
+        vector<bool> have_keys(n, false);
+        vector<bool> have_boxes(n, false);
+        vector<bool> visited(n, false);
+        int total_candies=0;
 
        for(int box: initialBoxes){
-        have_boxes.insert(box);
+        have_boxes[box]=true;
         if(status[box]==1) q.push(box);
        }
 
@@ -16,26 +17,25 @@ public:
         int temp_box=q.front();
         q.pop();
 
-        if (visited.contains(temp_box)) continue;
-        if (status[temp_box]==0 && !have_keys.contains(temp_box)) continue;
+        if (visited[temp_box]) continue;
+        visited[temp_box]=true;
 
-        visited.insert(temp_box);
         total_candies+=candies[temp_box];
 
         for (int temp_key : keys[temp_box]){
-            if (!have_keys.contains(temp_key)){
-                have_keys.insert(temp_key);
-                if( have_boxes.contains(temp_key) && !visited.contains(temp_key)){
+            if (!have_keys[temp_key]){
+                have_keys[temp_key]=true;
+                if( have_boxes[temp_key] && !visited[temp_key]){
                     q.push(temp_key);
                 }
             }
         }
 
         for(int temp_contained_box : containedBoxes[temp_box]){
-            if(!have_boxes.contains(temp_contained_box)){
-                have_boxes.insert(temp_contained_box);
+            if(!have_boxes[temp_contained_box]){
+                have_boxes[temp_contained_box] = true;
             }
-            if(!visited.contains(temp_contained_box) && (status[temp_contained_box]==1 || have_keys.contains(temp_contained_box))){
+            if(!visited[temp_contained_box] && (status[temp_contained_box]==1 || have_keys[temp_contained_box])){
                 q.push(temp_contained_box);
             }
         }
