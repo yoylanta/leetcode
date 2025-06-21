e275081db6ddcdd199b56c96d3b18d42ce659240
Iolanta Barysheva <82276707+yoylanta@users.noreply.github.com>
2025-06-21 17:46:40 +0300

    Time: 7 ms (63.41%), Space: 19.4 MB (55.07%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int minimumDeletions(string word, int k) {
-        unordered_map<char, int> freq_map;
-        for (char ch : word) ++freq_map[ch];
-        vector<int> freqs;
-        for (auto& [_, freq] : freq_map) freqs.push_back(freq);
-        int res = word.size();
-        for (int base : freqs) {
-            int del = 0;
-            for (int f : freqs) {
-                if (f < base) del += f;
-                else if (f > base + k) del += f - (base + k);
+        vector<int> freq(26, 0);
+        for (char c : word) ++freq[c - 'a'];
+        sort(freq.begin(), freq.end());
+        int total_deleted = 0, min_deletions = word.size();
+        for (int i = 0; i < 26; ++i) {
+            if (freq[i] == 0) continue;
+            int current = total_deleted;
+            for (int j = 25; j > i && freq[j] - freq[i] > k; --j) {
+                current += freq[j] - freq[i] - k;
             }
-            res = min(res, del);
+            min_deletions = min(min_deletions, current);
+            total_deleted += freq[i];
         }
-        return res;
+        return min_deletions;
     }
 };
