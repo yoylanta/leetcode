0ae187b44eca3b7a77dddba2d7a9bdddaaba3a8e
Iolanta Barysheva <82276707+yoylanta@users.noreply.github.com>
2025-06-23 16:25:43 +0300

    Time: 0 ms (100%), Space: 9.6 MB (78.6%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,23 +1,15 @@
 class Solution {
 private:
-    bool devides(string str1, string str2){
-        int n = str1.size(), m=str2.size();
-        if (n % m != 0) return false;
-        for(int i = 0; i < n; ++i){
-            if(str1[i]!=str2[i%m]) return false;
-        }
-        return true;
+    int gcd(int a, int b){
+        while(b!=0){
+            int t=a%b;
+            a=b; b=t;
+        } return a;
     }
 public:
     string gcdOfStrings(string str1, string str2) {
-        int maxGCDlen = min(str1.size(),str2.size());
-        string ans = "";
-        for(int len = 1; len<=maxGCDlen;++len){
-            if(str1.size()%len!=0 || str2.size()%len!=0) continue;
-            string prefix = str1.substr(0,len);
-            if(devides(str1, prefix) && devides(str2, prefix)){
-                if(prefix.size()>ans.size()) ans=prefix;
-            }
-        }return ans;
+        if(str1+str2!=str2+str1) return "";
+        int g= gcd(str1.size(),str2.size());
+        return str1.substr(0,g);
     }
 };
